e45fce68d25aa05d49ee889fe615c395117b201d
padhs <arosekpadhi@gmail.com>
2024-07-17 02:58:10 +0530

    created nashville table

--- nashville.sql
+++ nashville.sql
@@ -8,3 +8,26 @@ USE nashville_db;
 -- Use Python to automate INSERT INTO TABLE query for each record -- Moderate (Let's use this)
 -- Apache Spark: Create a JOB to Extract data from excel file into the nashville_db table -- Big Data
 
+-- Create table as per the data info from df in python script
+
+CREATE TABLE nashville(
+    UniqueID INT,
+    ParcelID VARCHAR(255),
+    LandUse VARCHAR(255),
+    PropertyAddress VARCHAR(255),
+    SaleDate DATE,
+    SalePrice INT,
+    LegalReference VARCHAR(255),
+    SoldAsVacant VARCHAR(255),
+    OwnerName VARCHAR(255),
+    OwnerAddress VARCHAR(255),
+    Acreage FLOAT,
+    TaxDistrict VARCHAR(255),
+    LandValue FLOAT,
+    BuildingValue FLOAT,
+    TotalValue FLOAT,
+    YearBuilt FLOAT,
+    Bedrooms FLOAT,
+    FullBath FLOAT,
+    HalfBath FLOAT
+)
--- nashville_data_import.py
+++ nashville_data_import.py
@@ -7,3 +7,12 @@ import mysql.connector
 df = pd.read_excel('./dataset/Nashville Housing Data for Data Cleaning.xlsx')
 print(df.info())
 # we get the columns and their datatypes it will be helpful while creating the table
+
+# establish connection to mysql db:
+connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='password',
+    database='nashville_db'
+)
+cursor = connection.cursor()
