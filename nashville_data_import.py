1fb01c94478c12219d5264bd76a0a4fda8b4887d
padhs <arosekpadhi@gmail.com>
2024-07-17 02:40:34 +0530

    data info

--- nashville_data_import.py
+++ nashville_data_import.py
@@ -1,1 +1,9 @@
 # import data from excel file to nashville_db
+
+import pandas as pd
+import mysql.connector
+
+# load the dataset into pandas pd
+df = pd.read_excel('./dataset/Nashville Housing Data for Data Cleaning.xlsx')
+print(df.info())
+# we get the columns and their datatypes it will be helpful while creating the table
