689281bff0d639682859668c14feedd1abbae94a
padhs <arosekpadhi@gmail.com>
2024-07-17 02:31:56 +0530

    nashville.sql

---
+++ nashville.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE nashville_db;
+
+USE nashville_db;
+
+-- Database Schema:
+-- step 1: We have excel file. Let's import data from dataset to the nashville_db
+-- We'll use the MySQL workbench for this (need to convert Excel --> csv text file) -- Easy
+-- Use Python to automate INSERT INTO TABLE query for each record -- Moderate (Let's use this)
+-- Apache Spark: Create a JOB to Extract data from excel file into the nashville_db table -- Big Data
+
